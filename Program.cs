b43928018f50a6546bf675f24fbec8fb2d8fbc73
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2025-07-26 04:32:05 -0300

    feat: adding migration but docker got broken

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using MyWallet.Features.Usuarios;
+using MyWallet.Models;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Monta a connection string dinamicamente
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MyWalletDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<UsuarioService>();
+
 var app = builder.Build();
+
+// Executa as migrations automaticamente no startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MyWalletDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
