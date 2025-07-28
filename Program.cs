752a8b2b7ca479b27da27261db3b411f3e5191ae
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2025-07-27 23:25:31 -0300

    fix: docker yml, adding nginx and it works \o/

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using MyWallet.Features.Usuarios;
 using MyWallet.Models;
 using System.Net;
@@ -21,19 +22,53 @@ builder.Services.AddScoped<UsuarioService>();
 
 var app = builder.Build();
 
-// Executa as migrations automaticamente no startup
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<MyWalletDbContext>();
-    db.Database.Migrate();
-}
-
-// Configure the HTTP request pipeline.
+// Wait for SQL Server to be ready
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<MyWalletDbContext>();
+        var maxAttempts = 30; // 30 seconds timeout
+        var attempts = 0;
+        
+        while (attempts < maxAttempts)
+        {
+            try
+            {
+                db.Database.CanConnect();
+                break;
+            }
+            catch
+            {
+                attempts++;
+                Thread.Sleep(1000); // Wait 1 second before next attempt
+            }
+        }
+        
+        if (attempts == maxAttempts)
+        {
+            throw new Exception("Could not connect to SQL Server after 30 attempts");
+        }
+
+        try
+        {
+            db.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while migrating the database.");
+            throw;
+        }
+        
+        
+    }
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
